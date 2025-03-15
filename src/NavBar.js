ad3c14e3eb2174dc2ff01414d9c8c9439f4324f1
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-15 14:15:18 +0530

    Added some css styles and deleted some files

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,10 +4,20 @@ const Navbar = () => {
             <h1>The Pawan Blog</h1>
             <div className="links">
                 <a href = "/">Home</a>
-                <a href = "/create">New Blog</a>
+                <a href = "/create" >New Blog</a>
             </div>
         </nav>
       );
 }
  
 export default Navbar;
+
+// we can do inline css style as we do in html.
+// we can also do css in a separate file and import it in our js file.
+// we can also use a library like bootstrap to make our css more efficient.
+// style={{
+//     color: 'white',
+//     backgroundColor: '#f1356d',
+//     border: 'none',
+//     borderRadius: '8px',
+// }}
