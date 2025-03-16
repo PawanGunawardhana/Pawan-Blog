5c278cf73eac41f30dff35f020596155208b1146
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-16 12:26:56 +0530

    Used Router Links

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,30 +6,30 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    console.log("user effect ran");
-    console.log(data);
+    //console.log("user effect ran");
+    //console.log(data);
     setTimeout(() => {
       fetch(url)
         .then((res) => {
-          console.log(res);
+         // console.log(res);
           if (!res.ok) {
             throw Error("Failed to fetch data from that API");
           }
           return res.json();
         })
         .then((data) => {
-          console.log(data);
+         // console.log(data);
           setIsPending(false);
           setData(data);
           setError(null);
         })
         .catch((err) => {
-          console.error(err);
+         // console.error(err);
           setError(err.message);
           setIsPending(false);
           setData(null);
         });
-    }, 500);
+    }, 1000);
   }, [url]);
 
   return { data, isPending, error };
