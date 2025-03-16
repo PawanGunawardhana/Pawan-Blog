165489f245ef7f4029c92b72bd05045a5580e255
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-16 11:38:41 +0530

    Define exact match routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import Navbar from './NavBar';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from "./NavBar";
+import Home from "./Home";
+import Create from "./Create";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-      <div className="content">
-        <Switch>
-          <Route path = "/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
